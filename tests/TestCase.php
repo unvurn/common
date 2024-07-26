0c866d0bd7946cb47bf86c7ba547ebf87b62a83a
hironobu koura <hironobu@unvurn.cc>
2024-07-26 22:05:34 +0900

    init

---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Unvurn\Common\Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
